3e363655253324ba36be4673cd9d1a742f767cf8
Tiago Rezende Campos Falcao <tiago@tiagofalcao.com>
2015-05-11 18:26:49 -0300

    New codejam test package

---
+++ codejam/test/test.go
@@ -0,0 +1,30 @@
+package test
+
+import (
+	"github.com/tiagofalcao/GoNotebook/codejam/manager"
+	"github.com/tiagofalcao/GoNotebook/diff"
+	"os"
+	"testing"
+)
+
+func callbackErr(base, received []byte, value interface{}) {
+	t := value.(*testing.T)
+	t.Errorf("Differ %s and %s", string(base), string(received))
+}
+
+func Test(t *testing.T, input string, caseTask manager.GCJCase, output string) {
+	i, err := os.Open(input)
+	if err != nil {
+		t.Errorf("Can't open %s", input)
+	}
+	b, err := os.Open(output)
+	if err != nil {
+		t.Errorf("Can't open %s", output)
+	}
+	o := diff.NewBuf(b, callbackErr, t)
+
+	manager.NewGCJManagerIO(caseTask, i, o).WaitEnd()
+
+	i.Close()
+	o.Close()
+}
