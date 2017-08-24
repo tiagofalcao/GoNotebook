90ef8c62e24d76278e370a77596b05aa5d8ddacd
Tiago Falcão <tiago@tiagofalcao.com>
2017-08-24 12:24:28 -0300

    Fix help message to -i and --input

--- manager/init.go
+++ manager/init.go
@@ -16,8 +16,8 @@ var FlushMode bool
 func init() {
 	flag.StringVar(&OptOutput, "output", "", "Output File")
 	flag.StringVar(&OptOutput, "o", "", "Output File")
-	flag.StringVar(&OptInput, "input", "", "Output File")
-	flag.StringVar(&OptInput, "i", "", "Output File")
+	flag.StringVar(&OptInput, "input", "", "Input File")
+	flag.StringVar(&OptInput, "i", "", "Input File")
 
 	flag.BoolVar(&FlushMode, "flush", true, "Force flush all output")
 }
