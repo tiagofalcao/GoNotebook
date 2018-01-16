a62c661a6bd740e389e4dc9c8bd08043df9e3ddf
Tiago Falcão <tiago@tiagofalcao.com>
2018-01-16 17:33:10 -0200

    Add UniqueLetters() to check duplicated letters ignoring case
    The CtCI book asked for non repeatable chars in string. This commit add
    similar function to the solution.

--- ctci/01_01/solution.go
+++ ctci/01_01/solution.go
@@ -3,6 +3,7 @@ package solution
 import (
 	"bufio"
 	"fmt"
+	"unicode"
 
 	"github.com/tiagofalcao/GoNotebook/log"
 )
@@ -10,6 +11,26 @@ import (
 func UniqueRunes(str string) bool {
 	m := make(map[rune]bool)
 	for i, c := range str {
+		_, ok := m[c]
+		if ok {
+			log.Debug.Printf("Duplicated %v at %d column", c, i)
+			return false
+		}
+		m[c] = true
+	}
+	return true
+}
+
+// UniqueLetter returns true if the string is composed by unique letters, ignoring the case.
+// Symbols can be repeated and are ignored.
+func UniqueLetters(str string) bool {
+	m := make(map[rune]bool)
+	for i, c := range str {
+		if !unicode.IsLetter(c) {
+			continue
+		}
+
+		c = unicode.ToLower(c)
 		_, ok := m[c]
 		if ok {
 			log.Debug.Printf("Duplicated %v at %d column", c, i)
