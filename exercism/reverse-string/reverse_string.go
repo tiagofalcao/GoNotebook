47b56a24663bceee0d91164a5008b812ee5b618d
Tiago Falcão <tiago@tiagofalcao.com>
2018-02-01 20:09:17 -0200

    Exercism: Reverse String

---
+++ exercism/reverse-string/reverse_string.go
@@ -0,0 +1,10 @@
+// Package reverse implements the reverse string.
+package reverse
+
+// Similar problem is presented in CtCI book.
+import solution "github.com/tiagofalcao/GoNotebook/ctci/01_02"
+
+// String returns the string s in reverse order.
+func String(s string) string {
+	return solution.Reverse(s)
+}
